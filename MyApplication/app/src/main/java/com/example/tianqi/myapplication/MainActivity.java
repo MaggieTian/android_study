fe88b1072e7ad2351962e0c1d675d7651986adec
Maggie Tian <1729430864@qq.com>
2018-03-13 08:13:24 +0800

    upload init code

---
+++ MyApplication/app/src/main/java/com/example/tianqi/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tianqi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //将xml布局文件引入到activity当中
+    }
+}
